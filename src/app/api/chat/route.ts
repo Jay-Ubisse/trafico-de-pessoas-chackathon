bbff1d0e8255931639995e81058d06f452639ec7
isaneyde <issitoe941@gmail.com>
2025-10-24 13:53:41 +0200

    feat: integrate AI SDK and enhance UI components
    - Added dependencies for AI SDK and UI components from Radix and Lucide.
    - Updated global styles to include new color variables and animations.
    - Refactored the main page to implement a chat dialog using the AI SDK.
    - Created reusable UI components for buttons, dialogs, inputs, and labels.
    - Implemented a chat API route to handle messages and responses from the AI model.
    - Established utility functions for class name management.

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,29 @@
+import { openai, createOpenAI } from '@ai-sdk/openai';
+import { streamText, UIMessage, convertToModelMessages } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+const lmstudio = createOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL: "http://127.0.0.1:1234/v1"
+})
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    //model: openai('gpt-4o'),
+    model: lmstudio("qwen/qwen3-8b"),
+    messages: convertToModelMessages(messages),
+    system: `
+        You are a virtual assistant working in the field of Human Trafficking. You check the text entered by the user and tell them whether it is human trafficking or not.
+
+        - Only answer questions related to human trafficking and violence.
+        - If the user enters a text whose content differs from these topics, tell them that you were created solely to answer questions about human trafficking and violence.
+        - Regardless of the language of the text entered, always answer in Portuguese.
+    `
+  });
+  console.log(result)
+  return result.toUIMessageStreamResponse();
+}
