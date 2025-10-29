69e3879bd1e2fd78963323073f811ce12df86b0c
bethynna2004 <dlambealbertina@gmail.com>
2025-10-29 10:30:40 +0200

    feat: implement complaints API with validation schema and database integration

---
+++ src/schemas/complaints.ts
@@ -0,0 +1,14 @@
+import {z} from "zod";
+
+export const complaintSchema = z.object({
+gender : z.enum(["Masculino", "Femenino"]),
+age :z.string().min(1, "Este campo  é  de preenchimento obrigatório")
+.max(2, "A idade deve ter no máximo 2 caracteres"),
+region : z.string().min(1, "A região é obrigatória"),
+purpose : z.string().min(1, "O propósito é obrigatório"),
+acction : z.string().min(1, "A ação é obrigatória"),
+location : z.string().min(1, "A localização é obrigatória"),
+
+
+
+})
